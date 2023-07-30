4fec6c60f75cee9163c9d3cb50c54aa9b8efde91
Satria-WS <extencezone02@gmail.com>
2023-07-31 06:46:25 +0700

    test

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import Product from "./pages/Product";
+import Home from "./pages/Home";
+import ProductList from "./pages/ProductList";
+import Register from "./pages/Register";
+import Login from "./pages/Login";
+import Cart from "./pages/Cart";
+
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route
+} from "react-router-dom";
+
+const App = () => {
+  return <Home/>;
+};
+
+export default App;
