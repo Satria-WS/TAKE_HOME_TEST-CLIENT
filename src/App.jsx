a7aa4f5282f0639fdf6cbe73f2a87d2beed51b3c
satria wibowo <extencezone02@gmail.com>
2023-07-31 12:14:01 +0700

    add

--- src/App.jsx
+++ src/App.jsx
@@ -7,12 +7,25 @@ import Cart from "./pages/Cart";
 
 import {
   BrowserRouter as Router,
+  Route,
   Routes,
-  Route
+  Navigate,
 } from "react-router-dom";
 
 const App = () => {
-  return <Home/>;
+  const user = false;
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/products/:category" element={<ProductList />} />
+        <Route path="product/:id" element={<Product />} />
+        <Route path="/cart" element={<Cart />} />
+        <Route path="/login" element={!user ? <Navigate to="/" /> : <Login />} />
+        <Route path="/register" element={<Register />} />
+      </Routes>
+    </Router>
+  );
 };
 
 export default App;
